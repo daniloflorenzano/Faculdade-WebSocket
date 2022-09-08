5c19a019de49f74451c68157e3fc4647f1796522
Danilo Florenzano <daniloflorenzano1@outlook.com>
2022-09-07 23:30:00 -0300

    refatora codigo

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var buffer = new byte[256]; 
+var buffer = new byte[256];
 var connections = new Dictionary<string, WebSocket>();
 
 app.UseWebSockets();
@@ -20,20 +20,17 @@ app.Map("/", async context =>
 
         while (true)
         {
-            var res = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
-            if (res.MessageType == WebSocketMessageType.Close)
-                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
-            else
+            foreach (var connection in connections) // broadcasting
             {
-                var recievedMessage = Encoding.ASCII.GetString(buffer, 0, res.Count);
-                var recievedMessageInBuffer =  Encoding.ASCII.GetBytes(recievedMessage);
-
-                foreach (var connection in connections) // broadcasting
+                if (connection.Value.State == WebSocketState.Open)
                 {
+                    var res = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                    var recievedMessage = Encoding.ASCII.GetString(buffer, 0, res.Count);
+                    var recievedMessageInBuffer = Encoding.ASCII.GetBytes(recievedMessage);
+
                     await connection.Value.SendAsync(recievedMessageInBuffer, WebSocketMessageType.Text, true, default);
                 }
-
-                await Task.Delay(1000);
             }
         }
     }
