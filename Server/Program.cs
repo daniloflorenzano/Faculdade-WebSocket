6723c8082884c77e332c01d1a5edf8434b21a257
daniloflorenzano <daniloflorenzano1@outlook.com>
2022-09-28 13:31:16 -0300

    Server: Cria log no console e trata erros

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,15 @@
 using System.Net;
 using System.Net.WebSockets;
 using System.Text;
+using Microsoft.AspNetCore.Connections;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var buffer = new byte[256]; 
+var buffer = new byte[256];
 var connections = new Dictionary<string, WebSocket>();
+
+Console.WriteLine(">>> SERVIDOR INICIANDO <<<");
 
 app.UseWebSockets();
 app.Map("/", async context =>
@@ -17,30 +20,49 @@ app.Map("/", async context =>
     {
         using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
         connections.Add(Guid.NewGuid().ToString(), webSocket);
-
-        while (true)
+        
+        Console.WriteLine($"{DateTime.Now} • Um cliente se conectou ao servidor");
+        Console.WriteLine($"{DateTime.Now} • Total de clientes conectados: {connections.Count}");
+        
+        try
         {
-            if (webSocket.State == WebSocketState.Closed)
-                continue;
-            
-            var res = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
-            
-            if (res.MessageType == WebSocketMessageType.Close)
-                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
-            else
+            while (true)
             {
-                var recievedMessage = Encoding.ASCII.GetString(buffer, 0, res.Count);
-                var recievedMessageInBuffer =  Encoding.ASCII.GetBytes(recievedMessage);
-
-                foreach (var connection in connections) // broadcasting
+                var closedConnection = connections.Where(c => c.Value.State == WebSocketState.Closed).FirstOrDefault();
+                if (closedConnection.Key != null)
                 {
-                    if (connection.Value.State == WebSocketState.Open)
-                        await connection.Value.SendAsync(recievedMessageInBuffer, WebSocketMessageType.Text, true, default);
-                    
-                    else
-                        connections.Remove(connection.Key);
+                    connections.Remove(closedConnection.Key);
+                    Console.WriteLine($"{DateTime.Now} • Um cliente se desconectou do servidor");
+                    Console.WriteLine($"{DateTime.Now} • Total de clientes conectados: {connections.Count}");
+                }
+                
+                if (webSocket.State == WebSocketState.Closed)
+                    continue;
+
+                var res = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                if (res.MessageType == WebSocketMessageType.Close)
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+                else
+                {
+                    var recievedMessage = Encoding.ASCII.GetString(buffer, 0, res.Count);
+                    var recievedMessageInBuffer = Encoding.ASCII.GetBytes(recievedMessage);
+
+                    foreach (var connection in connections) // broadcasting
+                    {
+                        if (connection.Value.State == WebSocketState.Open)
+                            await connection.Value.SendAsync(recievedMessageInBuffer, WebSocketMessageType.Text, true, default);
+                    }
                 }
             }
+        }
+        catch (WebSocketException e)
+        {
+            Console.WriteLine($"{DateTime.Now} • ERRO: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{DateTime.Now} • ERRO: {e.Message}");
         }
     }
 });
